735be9a589642b9c9fbdcc0239555bda49f48292
Zsolt <oszika05@gmail.com>
2018-11-25 19:21:17 +0100

    children to getUser

--- model/user.js
+++ model/user.js
@@ -27,14 +27,23 @@ module.exports = {
               AND scope = ?
         `, [groupId, role]),
 
-    getUserById: (userId) => new Promise((resolve, reject) => {
-        query(`SELECT * FROM user WHERE id = ?`, [userId])
-            .then(users => {
-                if (users.length === 0) return reject(responses.notFound())
+    getUserById: (userId) => new Promise(async (resolve, reject) => {
+        try {
+            const users = await query(`SELECT *
+                   FROM user
+                   WHERE id = ?`, [userId])
 
-                resolve(users[0])
+            if (users.length === 0) return reject(responses.notFound())
+
+            const children = await query(`SELECT * FROM child WHERE parentId = ?`, [users[0]['id']])
+
+            resolve({
+                ...users[0],
+                children: children
             })
-            .catch(reject)
+        } catch (error) {
+            reject(error)
+        }
     }),
 
     createUser: (user) => new Promise ((resolve, reject) => {
